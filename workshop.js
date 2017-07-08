55bf002e3b90e9b1abe3bf0fc2887cfb011b5a36
Ben Beatty <benbeatty85@gmail.com>
2017-07-08 17:36:47 +0000

    finished getAddressPosition

--- workshop.js
+++ workshop.js
@@ -12,7 +12,10 @@ function getIssPosition() {
     var issLocation = {}; //empty object for position information
       issLocation.lat = position.iss_position.latitude;
       issLocation.lng = position.iss_position.longitude;
-      console.log(issLocation);
+      //console.log(issLocation);
+  })
+  .catch(function(error) {
+    console.log("There was an error with your request");
   })
 }
 
@@ -20,8 +23,25 @@ getIssPosition();
 
 
 function getAddressPosition(address) {
-
+ var apiKey = "AIzaSyCSGYZwMFbCA1VftAGiJzlT-ZIz3pDphag";
+ var linkString = "https://maps.googleapis.com/maps/api/geocode/json?address=" + address + "&Key=" + apiKey;
+ 
+ return request(linkString)
+ .then(function(response) {
+   var googleAddress = JSON.parse(response);
+   var addressObj = {};
+   addressObj.lat = googleAddress.results[0].geometry.location.lat;
+   addressObj.lng = googleAddress.results[0].geometry.location.lng;
+   console.log(addressObj);
+   return addressObj;
+ })
+ .catch(function(error) {
+   console.log("There was an error");
+ })
+ 
 }
+
+getAddressPosition("665 Quaker Road Welland ON L3C 3H1");
 
 function getCurrentTemperatureAtPosition(position) {
 
