1ca5e78a42ed543edd60800f797a8efb17711342
Ben Beatty <benbeatty85@gmail.com>
2017-07-08 01:08:49 +0000

    work in progress

--- workshop.js
+++ workshop.js
@@ -6,11 +6,20 @@ function getDistance(pos1, pos2) {
 }
 
 function getIssPosition() {
-  return request(/* ... */)
+  return request('http://api.open-notify.org/iss-now.json')
   .then(
-    function(responce) {
+    function(response) {
+      
       // Parse as JSON
+      
+      var position = JSON.parse(response)
+      
       // Return object with lat and lng
+       var objPosition = {};
+       
+       objPosition.lat = position.iss_position.latitude;
+       objPosition.lng = position.iss_position.longitude;
+      
     }
   )
 }
